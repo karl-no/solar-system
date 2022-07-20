c945b0e8a3632043758a1e304c188f88a4b2ff7f
karl-no <karl.publicidade@gmail.com>
2022-07-20 20:41:43 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      // <p>Sistema Solar</p>
       <div>
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }
---
+++ src/components/Missions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import missions from '../data/missions';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h2>Missões</h2>
+        {/* <div>
+          {missions.map({ name, year, country, destination } => ())}
+        </div> */}
+      </div>
+    );
+  }
+}
+
+export default Missions;
